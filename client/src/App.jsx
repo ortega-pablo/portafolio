2d78261b68b239cee203a82e666bfcd756473712
ortega-pablo <p.ortegariera@gmail.com>
2022-09-08 21:27:26 -0300

    feat[SocialNav]: create SocialNav component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import Home from "./components/Home"
 import NavBar from "./components/NavBar"
+import SocialNav from "./components/SocialNav"
 
 function App() {
 	return (
 		<div>
 			<NavBar/>
 			<Home/>
+
+			<SocialNav/>
 		</div>
 	)
 }
