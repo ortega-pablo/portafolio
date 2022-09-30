09d74af314e8b143dcea2d7efb48c398f92b00d0
ortega-pablo <p.ortegariera@gmail.com>
2022-09-30 14:33:06 -0300

    changhe title and icon page

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -6,32 +6,31 @@ module.exports = {
 		extend: {
 			gridTemplateRows: {
 				// Simple 8 row grid
-				'8': 'repeat(8, minmax(0, 0.6fr))',
+				8: 'repeat(8, minmax(0, 0.6fr))',
 
 				// Complex site-specific row configuration
-				'layout': '200px minmax(900px, 1fr) 100px',
+				layout: '200px minmax(900px, 1fr) 100px',
 			},
 			colors: {
-				'bgDark': {
+				bgDark: {
 					1: '#001233',
 					2: '#001f54',
 				},
-				'bgLight': {
+				bgLight: {
 					1: '#fff0ef',
 					2: '#FDEEDD',
 				},
-				'primary': {
+				primary: {
 					1: '#0466c8',
 					2: '#0353a4',
 				},
-				'text': {
+				text: {
 					1: '#dcdcdd',
 					2: '#c5c3c6',
 					3: '#8b8c89',
 					4: '#4c5c68',
 					5: '#46494c',
 				},
-
 			},
 		},
 		fontFamily: {
